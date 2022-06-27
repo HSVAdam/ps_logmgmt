cfc3a74eb95aaef2888b3f983f1c0c93eca63808
abranham <adam.j.branham@gmail.com>
2022-06-27 14:04:24 -0500

    Initial Commit

---
+++ Backup-Folders.ps1
@@ -0,0 +1,160 @@
+#Requires -Modules 7Zip4Powershell
+
+<#
+    .SYNOPSIS
+    Provides folder based log management for application that do not transfer to long term storage.
+
+    .DESCRIPTION
+    This folder log management system is designed for applications that output logs into dated folders.  A
+    collection of all folders will be taken and processed by folder name in format of 'yyyyMMdd'.  Each folder
+    will be zipped and transfered to the destination location.
+
+    .PARAMETER Source
+    Root folder containing all of your dated folders.
+
+    .PARAMETER Destination
+    Destination location where you would all of your zip foldes to be located.
+
+    .PARAMETER AppName
+    This is simply an application or identifier name for your logs used in zip file name.
+
+    .PARAMETER KeepDays
+    The number of days you would like to leave untouched on the server.  These files will
+    not be zipped or moved to your destination location.
+
+    .PARAMETER CompressDrive
+    The temporary location to compress your files.  Compression over a network connection
+    or even to a seperate drive can be taxing.  This will compress then move your zip.
+    
+    .PARAMETER LogFolder
+    This is the location for this scripts logs.
+
+    .EXAMPLE
+    PS> .\Backup-Folders.ps1 -Source 'C:\inetpub\wwwroot\FOLDER\Folder2\Processed' -Destination 'D:\Backups' -AppName 'Folder_Archive-Processed'
+
+    .LINK
+    https://github.com/HSVAdam/ps_logmgmt
+#>
+
+PARAM (
+    [Parameter(Mandatory = $true, Position = 1)]
+    [ValidateScript({ IF (-Not ($_ | Test-Path)) { THROW 'Check source path.' } RETURN $true })]
+    [System.IO.FileInfo]$Source,
+    [Parameter(Mandatory = $true, Position = 2)]
+    [ValidateScript({ IF (-Not ($_ | Test-Path)) { THROW 'Check destination path.' } RETURN $true })]
+    [System.IO.FileInfo]$Destination,
+    [Parameter(Mandatory = $true)]
+    [string]$AppName,
+    [Parameter(Mandatory = $false)]
+    [int]$KeepDays = 14,
+    [Parameter(Mandatory = $false)]
+    [string]$CompressDrive = (Split-Path $Source -Qualifier),
+    [Parameter(Mandatory = $false)]
+    [ValidateScript({ IF (-Not ($_ | Test-Path)) { THROW 'Check log path.' } RETURN $true })]
+    [System.IO.FileInfo]$LogFolder = 'D:\Logs\Scripts'
+)
+
+# Set application name for logging
+$LogType = $AppName
+
+#region FUNCTIONS
+FUNCTION New-LogEntry {
+    [CmdletBinding()]
+    PARAM
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('Info', 'Error', 'Warn', 'Start', 'End')]
+        [string]$Level = 'Info',
+        [Parameter(Mandatory = $false)]
+        [string]$Path = $LogFolder,
+        [Parameter(Mandatory = $false)]
+        [switch]$NoClobber,
+        [Parameter(Mandatory = $false)]
+        [string]$Log = $LogType
+    )
+
+    BEGIN {
+        # Ensure log file exists, if not create it
+        $Today = Get-Date -Format 'yyyyMMdd'
+        $Year = Get-Date -Format 'yyyy'
+        $Month = Get-Date -Format 'MM'
+        IF (!(Test-Path -Path "$Path\$Log\$Year\$Month\$Log-$Today.log")) {
+            New-Item -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -ItemType File -Force | Out-Null
+            Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value '======================================================='
+            Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "File Created: [$Log]"
+            Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "File Date:    [$Today]"
+            Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "Log Path:     [$Path\$Log\$Year\$Month\$Log-$Today.log]"
+            Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "Server:       [$env:COMPUTERNAME]"
+            Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value '======================================================='
+            Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value ' '
+        }
+    }
+    PROCESS {
+        # Process supplied log data to file
+        $LogDate = Get-Date -UFormat '%x %r'
+        IF ($Level -eq 'Start') {
+            # Add a blank line to indicate new execution of script
+            Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value ''
+        }
+        Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "[$Level][$LogDate] $Message"
+        Write-Host "[$Level][$LogDate] $Message"
+    }
+    END {
+
+    }
+}
+#endregion FUNCTIONS
+
+New-LogEntry -Level 'Start' -Message 'Beginning logging process'
+
+TRY {
+    # Obtain list of all files in source folder older than $KeepDays and selecting only the required fields (FullName and LastWriteTimeUtc) This cuts down on memory usage
+    # Sort object by LastWriteTimeUtc in Date format, LastWriteTimeUtc is used for date creation accuracy no matter if file is moved/copied or not (Creation timestamp changes)
+    $Collection = Get-ChildItem -Path $Source | Where-Object { $_.PSIsContainer -eq 'true' -and [datetime]::parseexact($_.BaseName, 'yyyyMMdd', $null) -lt (Get-Date).AddDays(-$KeepDays) } | Select-Object FullName, Name, @{ Name = 'LastWriteTimeUtc'; Expression = { $_.LastWriteTimeUtc.ToString('yyyyMMdd') } }
+
+    New-LogEntry -Level 'Info' -Message "Source Location: $Source"
+    New-LogEntry -Level 'Info' -Message "Destination Location: $Destination"
+    New-LogEntry -Level 'Info' -Message "Keepdays: $KeepDays"
+    New-LogEntry -Level 'Info' -Message "Located $($Collection.Count) dates to manage"
+
+    # ForEach sorted date, compress to zip archive within local hidden TMP folder
+    # Once file compression is completed file will be moved to $Destination
+    # Once verification of file move is completed source files will be deleted
+    FOREACH ($Date in $Collection){
+        # Create TMP zip folder on source drive, thi folder will be hidden
+        # Zipping directly to $Destination could be over network, performing local zip is much faster
+        IF (!(Test-Path -Path (Join-Path -Path $CompressDrive -ChildPath 'TMP'))) {
+            New-Item -Path (Join-Path -Path $CompressDrive -ChildPath 'TMP') -ItemType Directory -Force
+            $TMPFolder = Get-Item (Join-Path -Path $CompressDrive -ChildPath 'TMP') -Force
+            $TMPFolder.Attributes='Hidden'
+        }
+
+        New-LogEntry -Level 'Info' -Message "Working on [$($Date.Name)] with [$($(Get-ChildItem -Path $Date.FullName).Count)] objects"
+
+        New-LogEntry -Level 'Info' -Message "Beginning Compression"
+        # Compress $Date.Group.FullName using 7Zip into zip using fastest compression
+        $DateFile = "$AppName-$($Date.Name).zip"
+        $Date.FullName | Compress-7Zip -ArchiveFileName (Join-Path -Path $CompressDrive -ChildPath "TMP\$DateFile") -Format Zip -CompressionLevel Fast
+        Move-Item -Path (Join-Path -Path $CompressDrive -ChildPath "TMP\$DateFile") -Destination $Destination -Force
+        New-LogEntry -Level 'Info' -Message 'Completed Compression'
+
+        # Ensure zip file has been created and is in destination location
+        IF (Test-Path -Path (Join-Path -Path $Destination -ChildPath $DateFile)) {
+            # Zip file has been located, cleanup source
+            New-LogEntry -Level 'Info' -Message 'Removing source files'
+            Remove-Item -Path $Date.FullName -Recurse -Force -Confirm:$false
+        } ELSE {
+            # Zip file creation failed, no source file removal
+        }
+    }
+}
+
+CATCH {
+    Write-Host $Error[0]
+    New-LogEntry -Level 'Error' -Message "Critical Error:  $($Error[0])"
+}
+
+New-LogEntry -Level 'End' -Message 'Completed logging process'
---
+++ Backup-OSLogs.ps1
@@ -0,0 +1,174 @@
+<#
+    .SYNOPSIS
+        OS level event log backup.
+    .DESCRIPTION
+        Copied Windows event log data from remote server to central backup location.  Default set to clear log after backup.
+    .PARAMETER DomainGroup
+        A domain security group which contains member servers to backup logs from.
+    .PARAMETER LogFiles
+        Windows event log files to backup from remote server.
+    .PARAMETER BackupRoot
+        Root location to backup files to.
+    .PARAMETER ClearLog
+        BOOL parameter to identify if you would like to log cleared after successful backup
+    .INPUTS
+        String (Hard-Coded Variables)
+    .OUTPUTS
+        None
+    .EXAMPLE
+        & Get-OSLogs.ps1
+    .LINK
+        https://github.com/HSVAdam/ps_logmgmt
+    .NOTES
+#>
+
+#region VARIABLES
+$DomainGroup = ''
+$LogFiles = @('System.evtx', 'Security.evtx', 'Application.evtx')
+$BackupRoot = ''
+$ClearLog = $true
+$Today = Get-Date -Format 'yyyyMMdd'
+
+#Script Logging Variables
+$LogFolder = 'D:\Logs\Scripts'  # Root log folder for script logging
+$LogType = 'OS-Logs'  # Defines the script name in the log folder
+#endregion VARIABLES
+
+#region FUNCTIONS
+FUNCTION New-LogEntry
+{
+	[CmdletBinding()]
+	PARAM
+	(
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Message,
+		[Parameter(Mandatory = $false)]
+		[ValidateSet("Info", "Error", "Warn", "Start", "End")]
+		[string]$Level = "Info",
+		[Parameter(Mandatory = $false)]
+		[string]$Path = $LogFolder,
+		[Parameter(Mandatory = $false)]
+		[switch]$NoClobber,
+		[Parameter(Mandatory = $false)]
+		[string]$Log = $LogType
+	)
+
+	BEGIN
+	{
+		# Ensure log file exists, if not create it
+		$Today = Get-Date -Format 'yyyyMMdd'
+		$Year = Get-Date -Format 'yyyy'
+		$Month = Get-Date -Format 'MM'
+		IF (!(Test-Path -Path "$Path\$Log\$Year\$Month\$Log-$Today.log"))
+		{
+			New-Item -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -ItemType File -Force | Out-Null
+			Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "======================================================="
+			Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "File Created: [$Log]"
+			Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "File Date:    [$Today]"
+			Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "Log Path:     [$Path\$Log\$Year\$Month\$Log-$Today.log]"
+			Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "Server:       [$env:COMPUTERNAME]"
+			Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "======================================================="
+			Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value " "
+		}
+	}
+	PROCESS
+	{
+		# Process supplied log data to file
+		$LogDate = Get-Date -UFormat '%x %r'
+		IF ($Level -eq 'Start')
+		{
+			# Add a blank line to indicate new execution of script
+			Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value ""
+		}
+		Add-Content -Path "$Path\$Log\$Year\$Month\$Log-$Today.log" -Value "[$Level][$LogDate] $Message"
+		Write-Host "[$Level][$LogDate] $Message"
+	}
+	END
+	{
+
+	}
+}
+#endregion FUNCTIONS
+
+TRY
+{
+    New-LogEntry -Message 'Beginning OS Log Processing' -Level Start
+
+    # Get listing of all computers from domain security group
+    IF ($ServerList = (Get-ADGroupMember -Identity $DomainGroup).Name)
+    {
+        New-LogEntry -Message "Obtained server list from [$DomainGroup]"
+        FOREACH ($Server in $ServerList)
+        {
+            # Update check name for local execution server
+            IF ($Server -eq $env:COMPUTERNAME) { $ServerName = 'localhost' } ELSE { $ServerName = $Server }
+            New-LogEntry "     Beginning [$Server]"
+            IF ((Test-NetConnection -ComputerName $ServerName -CommonTCPPort RDP -InformationLevel Quiet) -eq $true)
+            {
+                New-LogEntry -Message "          ONLINE"
+                # Server is responding, ensure backup folder exists
+                IF (!(Test-Path -Path "$BackupRoot\$Today\$Server"))
+                {
+                    New-Item -Path "$BackupRoot\$Today\$Server" -ItemType Directory -Force
+                    IF (!(Test-Path -Path "$BackupRoot\$Today\$Server"))
+                    {
+                        # Unable to create log backup folder, cancel all actions
+                        New-LogEntry -Message "     Unable to create backup log folder" -Level Error
+                        EXIT 1;
+                    }
+                }
+                # Log folder should now exist, begin file backups
+                FOREACH ($Log in $LogFiles)
+                {
+                    New-LogEntry -Message "     Working on [$Log]"
+                    IF (Test-Path -Path "\\$ServerName\c$\Windows\System32\winevt\Logs\$Log")
+                    {
+                        $CopyFile = Copy-Item -Path "\\$ServerName\c$\Windows\System32\winevt\Logs\$Log" -Destination "$BackupRoot\$Today\$Server" -PassThru
+                        IF ($CopyFile.Exists -eq $true -and (Test-Path -Path "$BackupRoot\$Today\$Server\$Log") -eq $true)
+                        {
+                            # File copied successfully, check if $ClearLog is $true
+                            New-LogEntry -Message "          Successful"
+                            IF ($ClearLog -eq $true)
+                            {
+                                $ClearLogFile = [io.path]::GetFileNameWithoutExtension($Log)
+                                New-LogEntry -Message "          Clearing [$ClearLogFile] from [$Server]"
+                                Clear-EventLog -LogName $ClearLogFile -ComputerName $ServerName
+                            }
+                            ELSE
+                            {
+                                # Not clearing event log on remote server
+                                New-LogEntry -Message "          Leaving event log intact on [$Server]"
+                            }
+                        }
+                    }
+                    ELSE
+                    {
+                        # Log file not found on remote server, possible SMB issue?
+                        New-LogEntry -Message "          Log File not found." -Level Error
+                    }
+                }
+            }
+            ELSE
+            {
+                # Server is not responding, could be check above, confirm Test-NetConnection command
+                New-LogEntry -Message "          OFFLINE" -Level Error
+            }
+        }
+    }
+    ELSE
+    {
+        # Unable to obtain server list from active directory security group
+        New-LogEntry -Message "Unable to obtain server list from domain security group." -Level Error
+    }
+}
+
+CATCH
+{
+    RETURN $Error[0]
+    New-LogEntry -Message $Error[0] -Level Error
+    EXIT 1;
+}
+
+New-LogEntry -Message 'Process Completed' -Level End
+EXIT 0;
